e9e0bf2d8a07a041863f7b47cc19a41aa0aabb48
Anton <polskij.anton707@gmail.com>
2023-10-27 00:10:43 +0300

    Homework-2

--- Test/seminars/second/hw/VehicleTest.java
+++ Test/seminars/second/hw/VehicleTest.java
@@ -1,15 +1,60 @@
 package seminars.second.hw;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class VehicleTest {
+    Car car;
+    Motorcycle motorcycle;
+    @BeforeEach
+    void setUp() {
+        car = new Car("Dodge", "Ram", 2010);
+        motorcycle = new Motorcycle("Honda", "EM1", 2010);
+    }
+
 
     @Test
     public void testCarIsInstanceOfVehicle() {
-        Car car = new Car("Dodge", "Ram", 2010);
         assertTrue(car instanceof Vehicle);
     }
+
+    @Test
+    public void testCarHasFourWheels() {
+        assertEquals(car.getNumWheels(),4);
+    }
+
+    @Test
+    public void testMotorcycleHasFourWheels() {
+        assertEquals(motorcycle.getNumWheels(),2);
+    }
+
+    @Test
+    public void testCarTestDriveSpeed() {
+        car.testDrive();
+        assertEquals(car.getSpeed(),60);
+    }
+
+    @Test
+    public void testMotorcycleTestDriveSpeed() {
+        motorcycle.testDrive();
+        assertEquals(motorcycle.getSpeed(),75);
+    }
+
+    @Test
+    public void testCarParkingSpeed() {
+        car.testDrive();
+        car.park();
+        assertEquals(car.getSpeed(),0);
+    }
+
+    @Test
+    public void testMotorcycleParkingSpeed() {
+        motorcycle.testDrive();
+        motorcycle.park();
+        assertEquals(motorcycle.getSpeed(),0);
+    }
+
 
 }
